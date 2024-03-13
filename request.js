50189d0d884ecff5966951a493c09c43055ef44f
Mairuzu1 <johnmiles23.jmr@gmail.com>
2024-03-13 18:54:16 +0800

    chore: Added more js files

--- request.js
+++ request.js
@@ -1,6 +1,69 @@
 import needle from 'needle';
 
 //needle.get('http://info.cern.ch/hypertext/WWW/TheProject.html', (err, res) => {
+needle.get('http://localhost:3000', (err, res) => {
+    console.log(res.body);   // prints the body of the response message. In this case, “Hello”
+});
+needle.get('http://localhost:3000/find-by-isbn-author', (err, res) => {
+    console.log(res.body);   // prints the body of the response message. In this case, “Hello”
+});
+
+needle.get('http://localhost:3000/find-by-author', (err, res) => {
+    console.log(res.body);   // prints the body of the response message. In this case, “Hello”
+});
+
+//needle.get('http://localhost:3000/find-by-author?author=J.K+Rowling', (err, res) => {
+//    console.log(res.body);   // prints the body of the response message. In this case, “Hello”
+
 needle.get('http://localhost:3000/greeting?name=Miles', (err, res) => {
     console.log(res.body);   // prints the body of the response message. In this case, “Hello”
 });
+
+/*needle.post(
+    'http://localhost:3000/submit-data',
+    { name: 'Monina' },   // can be an object or a string
+    (err, res) => {
+      console.log(res.body);
+    }
+  ); */
+
+//---------------------------------//
+
+needle.post(
+    'http://localhost:3000/add-book',
+    { name: "Harry Potter and the Philosopher’s Stone",
+    isbn: '978-0-7475-3269-9',
+    author: 'J.K Rowling',
+    year: '1997'},   // can be an object or a string
+    /*{ isbn: '978-0-7475-3269-9'},
+    { author: 'J.K Rowling'},
+    { year: '1997'},*/
+    (err, res) => {
+      console.log(res.body);
+    }
+  );  
+
+needle.post(
+    'http://localhost:3000/add-book',
+    { name: "Harry Potter and the Chamber of Secrets",
+    isbn: '0-7475-3849-2',
+    author: 'J.K Rowling',
+    year: '1998'},   // can be an object or a string
+    /*{ isbn: '0-7475-3849-2'},
+    { author: 'J.K Rowling'},
+    { year: '1998'},*/
+    (err, res) => {
+      console.log(res.body);
+    }
+  ); 
+  
+needle.post(
+    'http://localhost:3000/add-book',
+    { name: "The Little Prince" },   // can be an object or a string
+    /*{ isbn: '978-0156012195'},
+    { author: 'Antoine Saint-Exupery'},
+    { year: '1943'},*/
+    (err, res) => {
+      console.log(res.body);
+    }
+  );
--- server.js
+++ server.js
@@ -1,23 +1,74 @@
 import express from 'express';
+import { appendFileSync,readFile } from 'node:fs';
 
 // instantiate the server
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // this tells our app to listen for GET messages on the '/' path
 // the callback function specifies what the server will do when a message is received
+
+// HOMEPAGE
 app.get('/', (req, res) => {
     //console.log(req);
     //console.log(res);
     res.send('Hello!');
 });
 
-app.get('/greeting', (req, res) => {
-  res.send('Hello ' + req.query.name);
+// localhost:3000/greeting
+app.get('/greeting', (req, res) => {   // GET; ?name=Miles
+  res.send('Hello ' + req.query.name); //can be added to post 
 });
 
-app.post('/submit-data', (req, res) => {
-    res.send('Received a POST request.');
-  });
+// Endpoint for /find-by-author
+/*app.get('/find-by-author', (req,res) => {
+  //res.send('Author' + req.query.author);
+  readFile('book.txt', 'utf8', (err,data) =>{
+    if (err) throw err;
+    //console.log(data);
+
+    var lines = data.split('\n');
+    var result=[];
+
+    lines.forEach(line) => {
+      var line_element = line.split(',');
+
+      if(line_element.indexOf(req.query.author) == 2){
+        console.log('Found it: ' + line);
+        result.push(line);
+      }
+    }*/
+
+/* node request.js
+// Received a POST request from Monina
+app.post('/submit-data', (req, res) => {  //POST; {name: 'Monina'}
+    res.send('Received a POST request from ' + req.body.name);
+  });*/
+ 
+
+//localhost:3000/add-book
+app.post('/add-book', (req, res) => {  //POST; {name: 'Harry Potter'}
+  res.send(req.body.name,req.body.isbn,req.body.author,req.body.year)
+});
+
+// Function to add books
+function addBook([name, isbn, author, year]) {
+  //Checks if all the fields are present: first name, last name, and email are non-empty strings
+  // isEmail is added, and the age is at least 18
+  if (name && isbn && author && year){
+      // Let the data be written to books.txt
+      let data = `${name},${isbn},${author},${year}\n`;
+      // Append the data to users.txt
+      appendFileSync('books.txt', data);
+  }
+} 
+//res.send(req.body.name + req.body.isbn + req.body.author + req.body.year);
+
+//Append to books.txt
+addBook(["Harry Potter and the Philosopher’s Stone", "978-0-7475-3269-9", "J.K Rowling", 1997]);
+addBook(["Harry Potter and the Chamber of Secrets", "0-7475-3849-2", "J.K Rowling", 1998]);
+addBook(["The Little Prince", "978-0156012195", "Antoine Saint-Exupery", 1943]);
 
 // this tells our server to listen to the port 3000
 // we can also pass an optional callback function to execute after the server starts
